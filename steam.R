1b159ecd658b97d44cef75a462ed933007a87232
drewlake <lakegm@gmail.com>
2015-10-27 14:11:18 +0000

    first bit

---
+++ steam.R
@@ -0,0 +1,13 @@
+
+  
+  json_file1 <- "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="
+  Json_file2<-  "&steamid=76561197973625640&include_appinfo=1&format=json"
+  json_file <-paste(json_file1,key,Json_file2)
+  
+  json_data <- fromJSON(paste(readLines(json_file), collapse=""))
+  gamestatshttp<-"http://steamcommunity.com/profiles/76561197973625640/stats/440&format=json"
+  gamestats <- fromJSON(paste(readLines(gamestatshttp), collapse=""))
+  acheivmenthttp <- "http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=203770&format=json"
+  acheivment <-  fromJSON(paste(readLines(acheivmenthttp), collapse=""))
+  'pick a random game by weighted by low play time'
+  
