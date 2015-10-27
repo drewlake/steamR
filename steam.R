ce9f0c0e0274500addeb1ac37c3b1ff8d9f0ddb8
drewlake <lakegm@gmail.com>
2015-10-27 14:29:46 +0000

    Update steam.R

--- steam.R
+++ steam.R
@@ -1,8 +1,9 @@
 
-steamgames <- function () {
   
-  key <- "11F4F83E9C4054AAEFDF8AF401258C30"
-  json_file <- "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=11F4F83E9C4054AAEFDF8AF401258C30&steamid=76561197973625640&include_appinfo=1&format=json"
+  json_file1 <- "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="
+  Json_file2<-  "&steamid=76561197973625640&include_appinfo=1&format=json"
+  json_file <-paste(json_file1,key,Json_file2)
+  
   json_data <- fromJSON(paste(readLines(json_file), collapse=""))
   gamestatshttp<-"http://steamcommunity.com/profiles/76561197973625640/stats/440&format=json"
   gamestats <- fromJSON(paste(readLines(gamestatshttp), collapse=""))
@@ -10,4 +11,3 @@ steamgames <- function () {
   acheivment <-  fromJSON(paste(readLines(acheivmenthttp), collapse=""))
   'pick a random game by weighted by low play time'
   
-}
