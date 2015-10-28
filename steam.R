15416a3ad2d56e1d71939d52ee3d7af065ee8d5d
drewlake <lakegm@gmail.com>
2015-10-28 07:52:07 +0000

    fixed warning

--- steam.R
+++ steam.R
@@ -1,4 +1,6 @@
 gamelist <- function (key,id = "76561197973625640") {
+  warnings()
+  'needs seperate key from steam'
   library("rjson", lib.loc = "~/R/win-library/3.2")
   json_file1 <-
     "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="
@@ -6,6 +8,5 @@ gamelist <- function (key,id = "76561197973625640") {
   json_file3 <- "&include_appinfo=1&format=json"
   json_file <-
     paste(json_file1,key,json_file2,id,json_file3,sep = "")
-  
-  json_data <- fromJSON(paste(readLines(json_file), collapse = ""))
+  suppressWarnings (json_data <- fromJSON(paste(readLines(json_file), collapse = "")))
 }
