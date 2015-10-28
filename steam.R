ef61a453897e7e0508d16cd1509f7a9cbbee1313
drewlake <lakegm@gmail.com>
2015-10-28 09:18:10 +0000

    Removed bits of list

--- steam.R
+++ steam.R
@@ -1,6 +1,5 @@
+
 gamelist <- function (key,id = "76561197973625640") {
-  warnings()
-  'needs seperate key from steam'
   library("rjson", lib.loc = "~/R/win-library/3.2")
   json_file1 <-
     "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="
@@ -8,5 +7,8 @@ gamelist <- function (key,id = "76561197973625640") {
   json_file3 <- "&include_appinfo=1&format=json"
   json_file <-
     paste(json_file1,key,json_file2,id,json_file3,sep = "")
-  suppressWarnings (json_data <- fromJSON(paste(readLines(json_file), collapse = "")))
+  
+  list <-suppressWarnings(json_data <- fromJSON(paste(readLines(json_file), collapse = "")))
+  list$response$games
 }
+
