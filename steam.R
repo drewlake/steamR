5f24320a1b4eec4a536e3c34833c1857555de062
drewlake <lakegm@gmail.com>
2015-11-02 12:26:21 +0000

    Changed to dataframe

--- steam.R
+++ steam.R
@@ -1,6 +1,8 @@
 
+
 gamelist <- function (key,id = "76561197973625640") {
   library("rjson", lib.loc = "~/R/win-library/3.2")
+  library("data.table", lib.loc="~/R/win-library/3.2")
   json_file1 <-
     "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="
   json_file2 <-  "&steamid="
@@ -8,13 +10,24 @@ gamelist <- function (key,id = "76561197973625640") {
   json_file <-
     paste(json_file1,key,json_file2,id,json_file3,sep = "")
   
-  list <-suppressWarnings(fromJSON(paste(readLines(json_file), collapse = "")))
+  list <-
+    suppressWarnings(fromJSON(paste(readLines(json_file), collapse = "")))
   list$response$games
-  
+  rbindlist(list$response$games,fill=TRUE)
 }
-  
-gamedata <- function (key, gameid = "220"){
-url <- paste( "http://store.steampowered.com/api/appdetails/?appids=",gameid,sep="")
 
-list <-suppressWarnings(fromJSON(paste(readLines(url), collapse = "")))
-}
+
+gamedata <- function (gameid = "220") {
+  library("rjson", lib.loc = "~/R/win-library/3.2")
+  library("data.table", lib.loc="~/R/win-library/3.2")
+  url <-
+    paste("http://store.steampowered.com/api/appdetails/?appids=",gameid,sep =
+            "")
+  
+  list <-
+    suppressWarnings(fromJSON(paste(readLines(url), collapse = "")))
+  list<-list[[gameid]]
+  list<-list$data
+  rbindlist(list,fill=TRUE)
+  
+  }
