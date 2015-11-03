ed374e487ba5594c93ecf9fe842df4fc531e78a0
drewlake <lakegm@gmail.com>
2015-11-03 10:00:45 +0000

    added gameprice

--- steam.R
+++ steam.R
@@ -29,4 +29,18 @@ gamedata <- function (gameid = "220") {
   list<-list[[gameid]]
   list<-list$data
   
-  }
+}
+
+gameprice <- function (gameid = "220") {
+  library("rjson", lib.loc = "~/R/win-library/3.2")
+  library("data.table", lib.loc="~/R/win-library/3.2")
+  url <-
+    paste("http://store.steampowered.com/api/appdetails/?appids=",gameid,sep =
+            "")
+  
+  list <-
+    suppressWarnings(fromJSON(paste(readLines(url), collapse = "")))
+  list<-list[[gameid]]
+  list<-list$data$price_overview$final
+  
+}
