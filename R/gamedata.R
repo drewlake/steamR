92173951fddce33ed7d58929aef75f0198ca6149
drewlake <lakegm@gmail.com>
2015-11-04 12:30:57 +0000

    added functions

---
+++ R/gamedata.R
@@ -0,0 +1,14 @@
+
+gamedata <- function (gameid = "220") {
+  library("rjson", lib.loc = "~/R/win-library/3.2")
+  library("data.table", lib.loc = "~/R/win-library/3.2")
+  url <-
+    paste("http://store.steampowered.com/api/appdetails/?appids=",gameid,sep =
+            "")
+  
+  list <-
+    suppressWarnings(fromJSON(paste(readLines(url), collapse = "")))
+  list <- list[[gameid]]
+  list <- list$data
+  
+}
---
+++ R/gameprice.R
@@ -0,0 +1,12 @@
+gameprice <- function (gameid = "220") {
+  library("rjson", lib.loc = "~/R/win-library/3.2")
+  library("data.table", lib.loc = "~/R/win-library/3.2")
+  paste("http://store.steampowered.com/api/appdetails/?appids=",gameid,sep =
+          "")
+  
+  list <-
+    suppressWarnings(fromJSON(paste(readLines(url), collapse = "")))
+  list <- list[[gameid]]
+  list <- list$data$price_overview$final
+  
+}
