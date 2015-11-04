20bb6fdd899998424c5482917f91f95b0c415b13
drewlake <lakegm@gmail.com>
2015-11-04 12:28:31 +0000

    Redesign to a package

---
+++ R/games.R
@@ -0,0 +1,15 @@
+games <- function (key,id = "76561197973625640") {
+  library("rjson", lib.loc = "~/R/win-library/3.2")
+  library("data.table", lib.loc = "~/R/win-library/3.2")
+  json_file1 <-
+    "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="
+  json_file2 <-  "&steamid="
+  json_file3 <- "&include_appinfo=1&format=json"
+  json_file <-
+    paste(json_file1,key,json_file2,id,json_file3,sep = "")
+  
+  list <-
+    suppressWarnings(fromJSON(paste(readLines(json_file), collapse = "")))
+  list$response$games
+  rbindlist(list$response$games,fill = TRUE)
+}
