800368eae3f43868cd8d6b1766850889e05bb726
drewlake <lakegm@gmail.com>
2015-11-06 13:27:23 +0000

    minor fix & man

--- R/gameprice.R
+++ R/gameprice.R
@@ -1,15 +1,14 @@
 #' Game Price
 #' 
 #' Price of a game
+#' @note Maximum of 200 per 5 mins
 #' @export
 gameprice <- function (gameid = "220") {
   library("rjson", lib.loc = "~/R/win-library/3.2")
   library("data.table", lib.loc = "~/R/win-library/3.2")
   url<-paste("http://store.steampowered.com/api/appdetails/?appids=",gameid,sep =
           "")
-  
-  list <-
-    suppressWarnings(fromJSON(paste(readLines(url), collapse = "")))
+  list <- suppressWarnings(fromJSON(paste(readLines(url), collapse = "")))
   list <- list[[gameid]]
   list <- list$data$price_overview$final
   
