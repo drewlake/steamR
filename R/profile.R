f56e22b62ef33624e499ca0cf9f394038f110cdd
drewlake <lakegm@gmail.com>
2015-12-08 08:50:25 +0000

    Added Profile

---
+++ R/profile.R
@@ -0,0 +1,22 @@
+
+
+#' Steam Profile
+#' 
+#' gets steam profile, needs a valid steam api key
+#' @export
+stmprofile <- function (key,id = "76561197973625640") {
+  library("rjson", lib.loc = "~/R/win-library/3.2")
+  library("data.table", lib.loc = "~/R/win-library/3.2")
+  json_file1 <-
+    "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key="
+  
+  json_file2 <- "&steamids="
+  json_file3<- "&format=json"
+  json_file <-
+    paste(json_file1,key,json_file2,id,json_file3,sep = "")
+  
+  list <-
+    suppressWarnings(fromJSON(paste(readLines(json_file), collapse = "")))
+  list
+  rbindlist(list$response$players,fill = TRUE)
+}
