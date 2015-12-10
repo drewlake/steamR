6a25fbbb6c295a2b2f8094d68fb21a3b925ebcf5
drewlake <lakegm@gmail.com>
2015-12-10 11:06:12 +0000

    Added achievments

--- R/achievments.R
+++ R/achievments.R
@@ -1,20 +1,20 @@
-#' Steam Friends
+#' Steam achievments
 #' 
-#' get friends list needs key
+#' get steam achievment list
 #' @export
-friend <- function (key,id = "76561197973625640") {
+stmAchieve<- function (key,id = "76561197973625640") {
   library("rjson", lib.loc = "~/R/win-library/3.2")
   library("data.table", lib.loc = "~/R/win-library/3.2")
   json_file1 <-
-    "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key="
+    "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=440&key="
   
   json_file2 <- "&steamid="
-  json_file3<- "&relationship=friend&format=json"
+  json_file3<- "format=json"
   json_file <-
     paste(json_file1,key,json_file2,id,json_file3,sep = "")
   
   list <-
     suppressWarnings(fromJSON(paste(readLines(json_file), collapse = "")))
   list
-  rbindlist(frnds$friendslist$friends,fill = TRUE)
+  rbindlist(list$playerstats,fill = TRUE)
 }
