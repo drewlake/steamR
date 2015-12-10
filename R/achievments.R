0d9b33a9d065e277e0029938d9d2ac493ea3ff49
drewlake <lakegm@gmail.com>
2015-12-10 11:24:34 +0000

    edit

--- R/achievments.R
+++ R/achievments.R
@@ -1,20 +1,20 @@
-#' Steam achievments
+#' User stats for a game
 #' 
-#' get steam achievment list
+#' Returns list for user stats for a game
 #' @export
-stmAchieve<- function (key,id = "76561197973625640") {
+glbstats <- function (app = "17740", key, steamid="76561197972495328") {
   library("rjson", lib.loc = "~/R/win-library/3.2")
   library("data.table", lib.loc = "~/R/win-library/3.2")
   json_file1 <-
-    "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=440&key="
-  
-  json_file2 <- "&steamid="
-  json_file3<- "format=json"
-  json_file <-
-    paste(json_file1,key,json_file2,id,json_file3,sep = "")
+    "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid="
+  json_file2 <- "&key="
+  json_file3 <- "&steamid="
+  json_file <- 
+    paste(json_file1,app,json_file2,key,json_file3,steamid,sep = "")
   
   list <-
     suppressWarnings(fromJSON(paste(readLines(json_file), collapse = "")))
-  list
-  rbindlist(list$playerstats,fill = TRUE)
+  
+  #rbindlist(list$achievementpercentages$achievements,fill = TRUE)
+  
 }
